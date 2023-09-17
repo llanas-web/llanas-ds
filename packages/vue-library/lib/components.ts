aa5ca53168d6a9fb3c202f4a07914e539b56454b
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2023-09-17 19:06:30 +0200

    👷 pnpm stencil build

---
+++ packages/vue-library/lib/components.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated vue proxies */
+import { defineContainer } from './vue-component-lib/utils';
+
+import type { JSX } from 'stencil-library';
+
+
+
+
+export const LdsSwiper = /*@__PURE__*/ defineContainer<JSX.LdsSwiper>('lds-swiper', undefined, [
+  'name'
+]);
+
+
+export const MyComponent = /*@__PURE__*/ defineContainer<JSX.MyComponent>('my-component', undefined, [
+  'first',
+  'middle',
+  'last'
+]);
+
